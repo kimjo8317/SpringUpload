f054a4dbc8c6fa26084067de956be15753556bff
kimjunoh <kimjo8317@gmail.com>
2023-06-19 22:37:27 +0900

    File upload , download

--- src/main/java/hello/upload/Controller/SpringUploadController.java
+++ src/main/java/hello/upload/Controller/SpringUploadController.java
@@ -19,27 +19,22 @@ import java.io.IOException;
 public class SpringUploadController {
     @Value("${file.dir}")
     private String fileDir;
-
     @GetMapping("/upload")
     public String newFile() {
         return "upload-form";
     }
-
     @PostMapping("/upload")
-    public String saveFile(
-            @RequestParam String itemName,
-            @RequestParam MultipartFile file, HttpServletRequest request) throws IOException {
-
+    public String saveFile(@RequestParam String itemName,
+                           @RequestParam MultipartFile file, HttpServletRequest
+                                   request) throws IOException {
         log.info("request={}", request);
         log.info("itemName={}", itemName);
         log.info("multipartFile={}", file);
-
         if (!file.isEmpty()) {
-            String fullpath = fileDir + file.getOriginalFilename(); //업로드 파일명 지정 메서드
-            log.info("파일 저장 fullPath={}", fullpath);
-            file.transferTo(new File(fullpath)); //파일저장 메서드
+            String fullPath = fileDir + file.getOriginalFilename();
+            log.info("파일 저장 fullPath={}", fullPath);
+            file.transferTo(new File(fullPath));
         }
         return "upload-form";
     }
 }
-
